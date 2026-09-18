cec5c89e876663515d53e1a4a58305eaa67684c3
chanvate <drchanvate@gmail.com>
2026-09-18 08:39:38 +0700

    Update2 Java message

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming!");
+        System.out.println("Let's start coding!");
+        System.out.println("Happy coding!");
+        System.out.println("Stay positive and motivated!");
+        System.out.println("Embrace challenges and grow!");
+        System.out.println("Keep learning and improving!");
+        System.out.println("Never give up!");
     }
 }
