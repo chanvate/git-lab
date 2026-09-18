219fa99ebd4cb072ea6551c91fe8300b3859f15a
chanvate <drchanvate@gmail.com>
2026-09-18 08:46:21 +0700

    Update 5

--- Hello.java
+++ Hello.java
@@ -4,9 +4,5 @@ public class Hello {
         System.out.println("Welcome to Java programming!");
         System.out.println("Let's start coding!");
         System.out.println("Happy coding!");
-        System.out.println("Stay positive and motivated!");
-        System.out.println("Embrace challenges and grow!");
-        System.out.println("Keep learning and improving!");
-        System.out.println("Never give up!");
     }
 }
